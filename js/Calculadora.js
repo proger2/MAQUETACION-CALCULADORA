1bd79149f9ce8c081ccff4a6c393278dc661b037
Enzo Medina <enzo.gaston@itscordoba.edu.ar>
2023-06-23 14:51:19 -0300

    Se creó archivo calculadora

---
+++ js/Calculadora.js
@@ -0,0 +1,17 @@
+class Calculadora{
+    sumar(a,b){
+        return a + b;
+    }
+
+    restar(a,b){
+        return a - b;
+    }
+
+    multiplicar(a,b){
+        return a * b;
+    }
+
+    dividir(a,b){
+        return a / b;
+    }
+}
