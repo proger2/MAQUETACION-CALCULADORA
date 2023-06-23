d9c7b2e642bf827bc5639f7bdc92a9b4efbcb106
Enzo Medina <enzo.gaston@itscordoba.edu.ar>
2023-06-23 14:49:39 -0300

    Modificaciones en index,styles,mainjs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-const colores = ["azul","negro","verde"];
+/*CREO TODOS ELEMENTOS METIDOS EN VARIABLES PARA LUEGO UTILIZARLOS*/
+const displayValorAnterior = document.getElementById("valor-anterior")
+const displayValorActual = document.getElementById("valor-actual")
+const botonesnum = document.querySelectorAll("numero")
+const botonesoperadores = document.querySelectorAll("operador")
 
-colores.forEach((element) => console.log(element));
-    
+const display = new Display(displayValorActual,displayValorAnterior);
 
+botonesnum.forEach(boton =>{    /*por cada boton de numeros*/
+     boton.addEventListener("click",() => display.agregarNumero(boton.innerHTML));  /* el boton que clickee es el que se va a guardar en el metodo*/
+});
